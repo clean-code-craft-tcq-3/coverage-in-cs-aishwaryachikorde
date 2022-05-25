8b75d8c51873c1b1b617b04884bd811a2bdd925c
Aishwarya Chikorde <41798723+aishwaryachikorde@users.noreply.github.com>
2022-05-25 15:02:08 +0530

    Add files via upload

---
+++ TypewiseAlert/AlertTargets/ControllerAsTarget.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static TypewiseAlert.TypewiseAlert;
+using TypewiseAlert.Interfaces;
+
+namespace TypewiseAlert.AlertTargets
+{
+  public class ControllerAsTarget: IBreachAlert
+  {
+    public bool Send(BreachType breachType)
+    {
+      try
+      {
+        const ushort header = 0xfeed;
+
+        Console.WriteLine("{0} : {1}\n", header, breachType);
+
+        return true;
+
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine("Cannot be sent to controller: {0}", ex.Message);
+
+        return false;
+      }
+
+    }
+  }
+}
