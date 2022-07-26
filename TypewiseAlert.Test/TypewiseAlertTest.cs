905bfd49871fe687c465f8748951912c7984c201
Aishwarya Chikorde <41798723+aishwaryachikorde@users.noreply.github.com>
2022-07-26 14:48:36 +0530

    Update TypewiseAlertTest.cs

--- TypewiseAlert.Test/TypewiseAlertTest.cs
+++ TypewiseAlert.Test/TypewiseAlertTest.cs
@@ -13,9 +13,9 @@ namespace TypewiseAlert.Test
       Assert.True(TypewiseAlert.InferBreach(36, 30, 50) == TypewiseAlert.BreachType.NORMAL);
       Assert.True(TypewiseAlert.InferBreach(53, 30, 50) == TypewiseAlert.BreachType.TOO_HIGH);
       Assert.True(TypewiseAlert.InferBreach(30, 30, 50) == TypewiseAlert.BreachType.NORMAL);
-      Assert.True(TypewiseAlert.InferBreach(50, 30, 50) == TypewiseAlert.BreachType.TOO_HIGH);
+      Assert.True(TypewiseAlert.InferBreach(50, 30, 50) == TypewiseAlert.BreachType.NORMAL);
       Assert.True(TypewiseAlert.InferBreach(30, 30, 30) == TypewiseAlert.BreachType.NORMAL);
-     }
+    }
 
     [Fact]
     public void InfersBreachAsCoolingType()
@@ -23,37 +23,37 @@ namespace TypewiseAlert.Test
       Assert.True(TypewiseAlert.ClassifyTemperatureBreach(CoolingType.PASSIVE_COOLING, -1) == TypewiseAlert.BreachType.TOO_LOW);
       Assert.True(TypewiseAlert.ClassifyTemperatureBreach(CoolingType.PASSIVE_COOLING, 33) == TypewiseAlert.BreachType.NORMAL);
       Assert.True(TypewiseAlert.ClassifyTemperatureBreach(CoolingType.PASSIVE_COOLING, 56) == TypewiseAlert.BreachType.TOO_HIGH);
-      
+
       Assert.True(TypewiseAlert.ClassifyTemperatureBreach(CoolingType.MED_ACTIVE_COOLING, -3) == TypewiseAlert.BreachType.TOO_LOW);
       Assert.True(TypewiseAlert.ClassifyTemperatureBreach(CoolingType.MED_ACTIVE_COOLING, 25) == TypewiseAlert.BreachType.NORMAL);
-      Assert.True(TypewiseAlert.ClassifyTemperatureBreach(CoolingType.MED_ACTIVE_COOLING, 62) == TypewiseAlert.BreachType.TOO_HIGH);
-     
+      Assert.False(TypewiseAlert.ClassifyTemperatureBreach(CoolingType.MED_ACTIVE_COOLING, 40) == TypewiseAlert.BreachType.TOO_HIGH);
+
       Assert.True(TypewiseAlert.ClassifyTemperatureBreach(CoolingType.HI_ACTIVE_COOLING, -2) == TypewiseAlert.BreachType.TOO_LOW);
       Assert.True(TypewiseAlert.ClassifyTemperatureBreach(CoolingType.HI_ACTIVE_COOLING, 24) == TypewiseAlert.BreachType.NORMAL);
       Assert.True(TypewiseAlert.ClassifyTemperatureBreach(CoolingType.HI_ACTIVE_COOLING, 46) == TypewiseAlert.BreachType.TOO_HIGH);
-     }
+    }
 
-   [Fact]
+    [Fact]
     public void SendAlertsToEmailTests()
     {
       AlertContext alertContextToEmail = new AlertContext();
       alertContextToEmail.SetTarget(new EmailAsTarget());
 
-      
-      // Breach type with value TOO_LOW
+
+      //Breach type with value TOO_LOW
       alertContextToEmail.SetBreachType(BreachType.TOO_LOW);
       alertContextToEmail.Send();
-      Assert.True(alertContextToEmail.sent == true);
+      Assert.True(alertContextToEmail.sent);
 
-      // Breach type with value NORMAL
+      //Breach type with value NORMAL
       alertContextToEmail.SetBreachType(BreachType.NORMAL);
       alertContextToEmail.Send();
-      Assert.True(alertContextToEmail.sent == true);
+      Assert.True(alertContextToEmail.sent);
 
-      // Breach type with value TOO_HIGH
+      //Breach type with value TOO_HIGH
       alertContextToEmail.SetBreachType(BreachType.TOO_HIGH);
       alertContextToEmail.Send();
-      Assert.True(alertContextToEmail.sent == true);
+      Assert.True(alertContextToEmail.sent);
     }
 
     [Fact]
@@ -62,20 +62,20 @@ namespace TypewiseAlert.Test
       AlertContext alertContextToController = new AlertContext();
       alertContextToController.SetTarget(new ControllerAsTarget());
 
-      // Breach type with TOO_LOW
+      //Breach type with TOO_LOW
       alertContextToController.SetBreachType(BreachType.TOO_LOW);
       alertContextToController.Send();
-      Assert.True(alertContextToController.sent == true);
+      Assert.True(alertContextToController.sent);
 
-      // Breach type with NORMAL
+      //Breach type with NORMAL
       alertContextToController.SetBreachType(BreachType.NORMAL);
       alertContextToController.Send();
-      Assert.True(alertContextToController.sent == true);
+      Assert.True(alertContextToController.sent);
 
-      // Breach type with TOO_HIGH
+      //Breach type with TOO_HIGH
       alertContextToController.SetBreachType(BreachType.TOO_HIGH);
       alertContextToController.Send();
-      Assert.True(alertContextToController.sent == true);
+      Assert.True(alertContextToController.sent);
     }
   }
 }
